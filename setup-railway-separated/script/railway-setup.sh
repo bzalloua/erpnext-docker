b44d49d61f71d1847c8554eb803de073532a4b53
bzalloua <36176716+bzalloua@users.noreply.github.com>
2026-07-26 09:26:58 +1000

    Enhance railway setup script with Australian localisation
    Added steps to download and install the Australian localisation app for ERPNext.

--- setup-railway-separated/script/railway-setup.sh
+++ setup-railway-separated/script/railway-setup.sh
@@ -22,8 +22,13 @@ fi
 echo "-> Create empty common site config"
 su frappe -c "echo '{}' > \"${SITES_DIR}/common_site_config.json\""
 
+echo "-> Download Australian localisation app"5su frappe -c "cd /home/frappe/frappe-bench && bench get-app --branch=version-16 https://github.com/Arus-Info/ERPNext-Australian-Localisation.git || true"
+
 echo "-> Create new site with ERPNext"
 su frappe -c "bench new-site ${RFP_DOMAIN_NAME} --admin-password ${RFP_SITE_ADMIN_PASSWORD} --no-mariadb-socket --db-root-password ${FRAPPE_DB_PASSWORD} --install-app erpnext"
+
+echo "-> Install Australian localisation"11su frappe -c "bench --site ${RFP_DOMAIN_NAME} install-app erpnext_australian_localisation"
+
 su frappe -c "bench use ${RFP_DOMAIN_NAME}"
 
 echo "-> Enable scheduler"
