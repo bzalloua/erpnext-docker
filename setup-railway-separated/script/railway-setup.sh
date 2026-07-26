fcd2263f6a16e443b7255c3684f2872b239bae58
bzalloua <36176716+bzalloua@users.noreply.github.com>
2026-07-26 10:17:28 +1000

    Improve formatting and add localization checks in setup script
    Refactor railway setup script for clarity and structure.

--- setup-railway-separated/script/railway-setup.sh
+++ setup-railway-separated/script/railway-setup.sh
@@ -20,14 +20,24 @@ if [ -d "${SITES_DIR}/${RFP_DOMAIN_NAME}" ]; then
 fi
 
 echo "-> Create empty common site config"
-su frappe -c "echo '{}' > \"${SITES_DIR}/common_site_config.json\""
+su frappe -c "echo '{}' > '${SITES_DIR}/common_site_config.json'"
 
-echo "-> Download Australian localisation app"5su frappe -c "cd /home/frappe/frappe-bench && bench get-app --branch=version-16 https://github.com/Arus-Info/ERPNext-Australian-Localisation.git || true"
+echo "-> Download Australian localisation app"
+su frappe -c "cd /home/frappe/frappe-bench && bench get-app --branch=version-16 https://github.com/Arus-Info/ERPNext-Australian-Localisation.git || true"
+
+echo "-> Ensure apps.txt contains Australian localisation"
+grep -qxF "erpnext_australian_localisation" /home/frappe/frappe-bench/sites/apps.txt || \
+echo "erpnext_australian_localisation" >> /home/frappe/frappe-bench/sites/apps.txt
 
 echo "-> Create new site with ERPNext"
-su frappe -c "bench new-site ${RFP_DOMAIN_NAME} --admin-password ${RFP_SITE_ADMIN_PASSWORD} --no-mariadb-socket --db-root-password ${FRAPPE_DB_PASSWORD} --install-app erpnext"
+su frappe -c "bench new-site ${RFP_DOMAIN_NAME} \
+    --admin-password ${RFP_SITE_ADMIN_PASSWORD} \
+    --no-mariadb-socket \
+    --db-root-password ${FRAPPE_DB_PASSWORD} \
+    --install-app erpnext"
 
-echo "-> Install Australian localisation"11su frappe -c "bench --site ${RFP_DOMAIN_NAME} install-app erpnext_australian_localisation"
+echo "-> Install Australian localisation"
+su frappe -c "bench --site ${RFP_DOMAIN_NAME} install-app erpnext_australian_localisation"
 
 su frappe -c "bench use ${RFP_DOMAIN_NAME}"
 
